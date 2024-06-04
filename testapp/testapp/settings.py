1a43991a2cf115a2f02a9dd4e0812343ec2a8448
Fabio Pinto <fabio.pinto@blip.pt>
2024-06-04 16:45:58 +0100

    fixes settings

--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(str(BASE_DIR / "local.env"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,9 +101,6 @@ else:
         }
     }
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-AUTH_USER_MODEL = "testapp.User"
 
 
 # Password validation
