b0f213c04d672f0bd664eb2b4befa2cb13dea77f
Fábio Pinto <67961647+fpintoppb@users.noreply.github.com>
2025-07-10 10:54:03 +0100

    Django 5 support (#22)

--- sbomrepo/__init__.py
+++ sbomrepo/__init__.py
@@ -1,9 +1,11 @@
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 
 import os
 import sys
 
+
 def django_manage():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
     from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
